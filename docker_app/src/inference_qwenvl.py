c62364f0de004c6c7c54e30bedc132ea158057c2
brianestadimas <brians.estadimas@gmail.com>
2025-03-03 14:21:43 +0900

    Feat: Multimodal video infeence

--- docker_app/src/inference_qwenvl.py
+++ docker_app/src/inference_qwenvl.py
@@ -12,8 +12,6 @@ TOKENIZER = None
 
 def initialize_model(model_id: str, checkpoint_root: str = "./model_cp"):
     global MODEL, TOKENIZER
-
-    # If already loaded, just return
     if MODEL is not None and TOKENIZER is not None:
         return MODEL, TOKENIZER
 
@@ -27,17 +25,12 @@ def initialize_model(model_id: str, checkpoint_root: str = "./model_cp"):
     print("Loading base model...")
     model, tokenizer = FastVisionModel.from_pretrained(
         model_name =  model_name,  # Trained model either locally or from huggingface
-        load_in_4bit = False,
+        load_in_4bit = True,
     )
     print("Base model loaded.")
-
-    # 2. Find highest checkpoint
-
     MODEL = model
     TOKENIZER = tokenizer
-
     return MODEL, TOKENIZER
-
 
 def run_inference_qwenvl(image: Image.Image, user_input: str, temperature: float = 0.0, 
                         max_tokens: int = 500, model_id: str = "unsloth/Qwen2-VL-7B-Instruct") -> str:
@@ -58,7 +51,6 @@ def run_inference_qwenvl(image: Image.Image, user_input: str, temperature: float
             ]
         }
     ]
-    # Tokenize prompt using the built-in chat template
     input_text = tokenizer.apply_chat_template(messages, add_generation_prompt = True)
     inputs = tokenizer(
         image,
@@ -82,7 +74,7 @@ def run_inference_qwenvl(image: Image.Image, user_input: str, temperature: float
 
 
 def run_inference_qwenvl(image, user_input, temperature=0.0, max_tokens=500, model_id="unsloth/Qwen2-VL-7B-Instruct"):
-    model, tokenizer = FastVisionModel.from_pretrained(model_name=model_id, load_in_4bit=True, use_gradient_checkpointing=False)
+    model, tokenizer = initialize_model(model_id)
     FastVisionModel.for_inference(model)
 
     messages = [{"role": "user", "content": [{"type": "image"}, {"type": "text", "text": user_input}]}]
@@ -97,11 +89,11 @@ def run_inference_qwenvl(image, user_input, temperature=0.0, max_tokens=500, mod
 
 def run_inference_qwenvl_video(video_path="./Video.mp4", user_input="Describe video", temperature=0.0, 
                                max_tokens=500, model_id="unsloth/Qwen2-VL-7B-Instruct") -> str:
-    model, tokenizer = FastVisionModel.from_pretrained(model_name=model_id, load_in_4bit=True, use_gradient_checkpointing=False)
+    model, tokenizer = initialize_model(model_id)
     processor = AutoProcessor.from_pretrained(model_id)
     FastVisionModel.for_inference(model)
 
-    messages = [{"role": "user", "content": [{"type": "video", "video": f"{video_path}", "max_pixels": 360 * 420, "fps": 1.0}, 
+    messages = [{"role": "user", "content": [{"type": "video", "video": f"{video_path}", "max_pixels": 240 * 300, "fps": 1.0}, 
                                              {"type": "text", "text": user_input}]}]
 
     text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
@@ -109,6 +101,6 @@ def run_inference_qwenvl_video(video_path="./Video.mp4", user_input="Describe vi
     inputs = processor(text=[text], images=image_inputs, videos=video_inputs, padding=True, return_tensors="pt").to("cuda")
 
     with torch.no_grad():
-        generated_ids = model.generate(**inputs, max_new_tokens=max_tokens, use_cache=True, temperature=temperature, do_sample=False, min_p=0.1)
+        generated_ids = model.generate(**inputs, max_new_tokens=max_tokens, use_cache=True, temperature=temperature, do_sample=False, min_p=0.1, repetition_penalty=1.1,)
 
     return processor.batch_decode(generated_ids[:, inputs['input_ids'].shape[1]:], skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
