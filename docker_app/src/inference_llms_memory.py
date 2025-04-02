4ea6b2154bd9e90f338f6467018a601235bcdd99
brianestadimas <brians.estadimas@gmail.com>
2025-04-02 11:04:31 +0900

    Feat: RAG IMP - add model

--- docker_app/src/inference_llms_memory.py
+++ docker_app/src/inference_llms_memory.py
@@ -47,7 +47,7 @@ def initialize_model(model_id: str, checkpoint_root: str = "./model_cp", separat
     print(f"Loading model from: {model_name}")
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name=model_name,
-        load_in_4bit=True,
+        load_in_4bit=False,
     )
     RETRIEVER = retriever
     MODEL = model
